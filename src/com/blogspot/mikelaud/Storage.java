f8e80c237ca338cfc0e13a751177ab1f0634e90e
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-05-06 04:15:02 +0300

    add get/set values

--- src/com/blogspot/mikelaud/Storage.java
+++ src/com/blogspot/mikelaud/Storage.java
@@ -13,6 +13,34 @@ public class Storage {
 	private FileChannel mFileChannel = null;
 	private MappedByteBuffer mBuffer = null;
 	
+	private int mapLongToByteIndex(int aLongIndex) {
+		return aLongIndex * (Long.SIZE / Byte.SIZE);
+	}
+	
+	private int mapDoubleToByteIndex(int aDoubleIndex) {
+		return aDoubleIndex * (Double.SIZE / Byte.SIZE);
+	}
+	
+	public void setLong(int aLongIndex, long aValue) {
+		int byteIndex = mapLongToByteIndex(aLongIndex);
+		mBuffer.putLong(byteIndex, aValue);
+	}
+	
+	public long getLong(int aLongIndex) {
+		int byteIndex = mapLongToByteIndex(aLongIndex);
+		return mBuffer.getLong(byteIndex);
+	}
+	
+	public void setDouble(int aDoubleIndex, double aValue) {
+		int byteIndex = mapDoubleToByteIndex(aDoubleIndex);
+		mBuffer.putDouble(byteIndex, aValue);
+	}
+	
+	public double getDouble(int aDoubleIndex) {
+		int byteIndex = mapDoubleToByteIndex(aDoubleIndex);
+		return mBuffer.getDouble(byteIndex);
+	}
+	
 	public boolean isClosed() {
 		return (null == mBuffer || null == mFileChannel || null == mRandomAccessFile);
 	}
